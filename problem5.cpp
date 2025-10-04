7908e5fd8394477d3144f69440f6192928c6676d
Dilshodbek078 <d.orazbayev@newuu.uz>
2025-10-04 22:48:17 +0500

    Create problem5.cpp

---
+++ problem5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int count = 0;
+
+    for (char ch = 'A'; ch <= 'Z'; ch++) {
+        cout << ch << " ";
+        count++;
+
+        if (count % 5 == 0) {
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
